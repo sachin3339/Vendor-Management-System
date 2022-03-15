3d27100179f2784e4b4fe0726a442e80b6587a55
shreyesbenak <shreyesbenak@gmail.com>
2022-03-15 20:36:22 +0530

    login UI

---
+++ Client/vendor-management/src/components/signIn/SignIn.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+const Login=()=>{
+
+    const paperStyle={padding :20,height:'60vh',width:280, margin:"20px auto" , marginTop: '100px'}
+    const avatarStyle={backgroundColor:'#1bbd7e'}
+    const btnstyle={margin:'8px 0'}
+
+
+    const {userName , setUserName} = useState('')
+    const {password , setPassword} = useState('')
+
+    // const userNameHandler =  (e) = {
+
+    // }
+    // const pssWordHandler = ( e) = {
+        
+    // }
+    return(
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required onChang/>
+                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <FormControlLabel
+                    control={
+                    <Checkbox
+                        name="checkedB"
+                        color="primary"
+                    />
+                    }
+                    label="Remember me"
+                 />
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                <Typography >
+                     <Link href="#" >
+                        Forgot password ?
+                </Link>
+                </Typography>
+                <Typography > Do you have an account ?
+                     <Link href="#" >
+                        Sign Up 
+                </Link>
+                </Typography>
+            </Paper>
+        </Grid>
+    )
+}
+
+export default Login
